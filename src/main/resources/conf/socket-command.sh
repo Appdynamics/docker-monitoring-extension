38b03ed44ccc8d3c13abad324b99d0258a0076df
abey.tom <abey.tom@appdynamics.com>
2017-04-19 10:52:53 -0400

    switched to curl by default for unix sockets

--- src/main/resources/conf/socket-command.sh
+++ src/main/resources/conf/socket-command.sh
@@ -5,14 +5,14 @@
 ########################################################################
 #### Using NC: This will not work in docker 1.8.2 onwards          #####
 ########################################################################
-echo -e "GET $1 HTTP/1.0\r\n" | sudo nc -U /var/run/docker.sock
+#echo -e "GET $1 HTTP/1.0\r\n" | sudo nc -U /var/run/docker.sock
 
 # This can be used only in Debian. Will work for all versions of Docker
 #echo -e "GET $1 HTTP/1.0\r\n" | sudo nc -U -q 5 /var/run/docker.sock
 
 ########################################################################
-##### Using CURL: Needs CURL v1.40+                            #########
+##### Using CURL: Needs CURL v7.40+                            #########
 ##### This will not work with earlier versions of CURL         #########
 ########################################################################
-#sudo curl -s -S -i --unix-socket /var/run/docker.sock  http:$1
+sudo curl -s -S -i --unix-socket /var/run/docker.sock  http:$1
 
